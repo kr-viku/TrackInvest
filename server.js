23a495b9b19bed68caf208b01b2c5bcf20f072ef
kr.viku <er.vikashs4@gmail.com>
2020-08-24 23:45:34 +0530

    git

---
+++ server.js
@@ -0,0 +1,28 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const MongoClient=mongoose.MongoClient
+const app=express()
+
+const url = 'mongodb+srv://egnviku:db141528@cluster0-itvts.mongodb.net/TrackInvest?retryWrites=true&w=majority';
+
+
+app.post("/data", (req,res)=>{
+    var MongoClient = require('mongodb').MongoClient;
+
+
+    MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("TrackInvest");
+  dbo.collection("trackinvest").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    res.status(200).json({result})
+    db.close();
+  });
+});
+})
+
+app.listen(8000, (err)=>{
+    if(!err){
+        console.log("listen")
+    }
+})
